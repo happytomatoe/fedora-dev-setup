ed95b72580145eb3daee16291e30289ae9e4d44f
Roman Lukash <2893931+happytomatoe@users.noreply.github.com>
2025-09-09 09:10:09 +0200

    Init

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # fedora-fresh-start
 
-Set of scripts and instruction on how to configure fedora after install
+Set of scripts and instruction on how to configure fedora development environment after install. Configuration is very similar to https://www.youtube.com/watch?v=Wj1y_eAhlvc
+
+
+1) Run install.sh
+2) To configure manually
+- Disable animation: Go to system settings -> accessibility-> seeing-> Turn of animation effects
+- Open apps with hotkeys: Go to https://extensions.gnome.org/extension/1336/run-or-raise/ and install it. 
+- Install vimium chrome extension for browser
+
+
+## TODO
+- Add config for run or raise
+- Add fzf
+- Add vscode and intellij idea
---
+++ install.sh
@@ -0,0 +1,35 @@
+# terminal
+sudo dnf install alacrity
+sudo dnf install ghostty
+
+sudo dnf install zsh
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+
+#zsh plugins
+# 1)minimal:
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+# 2) Alternative - https://gist.github.com/n1snt/454b879b8f0b7995740ae04c5fb5b7df
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
+
+zsh -c "source ~/.zshrc"
+
+
+
+#terminal multiplexer
+sudo dnf copr enable varlad/zellij
+sudo dnf install zellij
+# browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
+#text editor
+sudo dnf install neovim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
