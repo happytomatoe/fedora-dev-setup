0a261f6eee65894092252801e44612fef4d70af7
happytomatoe <2893931+happytomatoe@users.noreply.github.com>
2026-05-02 13:59:38 +0200

    Use Alt+Number instead of Ctrl+Number for app switching

--- install.sh
+++ install.sh
@@ -178,10 +178,10 @@ echo "Disabling animation effects..."
 gsettings set org.gnome.desktop.interface enable-animations false
 gsettings set org.gnome.mutter enable-animations false 2>/dev/null || true
 
-# Ctrl+Number for app switching (instead of Super+Number)
-echo "Setting Ctrl+Alt+Number for app switching..."
+# Alt+Number for app switching (instead of Super+Number)
+echo "Setting Alt+Number for app switching..."
 for i in {1..9}; do
-  gsettings set org.gnome.shell.keybindings "switch-to-application-$i" "['<Primary>$i']"
+  gsettings set org.gnome.shell.keybindings "switch-to-application-$i" "['<Alt>$i']"
 done
 for i in {1..10}; do
   gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-$i" "[]"
